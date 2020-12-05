d340792fe8184189e0b1402ba9eb91d1ad23002a
Leamsi Escribano <leamsi.esc@gmail.com>
2020-12-05 17:16:09 -0500

    Adding api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { clientID, clientSecret } from './api';
 import './App.scss';
 
-function App() {
+const App: React.FC = () => {
+  async function getToken() {
+    const response = await fetch('https://api.petfinder.com/v2/oauth2/token', {
+      method: 'POST',
+      body: `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+    });
+
+    response.json().then(data => console.log(data));
+  }
+  
+  // useEffect(() => {getToken()}, []);
+
+
   return (
       <div className="App">
       </div>
